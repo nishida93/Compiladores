d9565fe1c0acd61e0f7ccdf907ad666b0169614f
Luan <bonomiluan@gmail.com>
2018-10-24 08:54:27 -0300

    mudnado a cor da mensagem de erro

--- compilador-infra/src/puc/compiladores/sintatico/SintaticoException.java
+++ compilador-infra/src/puc/compiladores/sintatico/SintaticoException.java
@@ -9,14 +9,14 @@ public class SintaticoException extends Exception {
 
 	public SintaticoException(final String s, final JTextArea textAreaErro, final JTextArea textAreaCodigo, final int linha) {
         DefaultHighlighter highlighter = (DefaultHighlighter)textAreaCodigo.getHighlighter();
-        DefaultHighlighter.DefaultHighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.RED);
+        DefaultHighlighter.DefaultHighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.getHSBColor(00,80,100));
         highlighter.setDrawsLayeredHighlights(true);
         try {
             int start = textAreaCodigo.getLineStartOffset(linha - 1);
             int end = textAreaCodigo.getLineEndOffset(linha - 1);
             highlighter.addHighlight(start, end, painter);
         } catch (Exception e) {
-            System.out.println("batata" + e);
+            System.out.println(e);
         }
 		textAreaErro.append(s);
 		textAreaErro.setForeground(Color.RED);
